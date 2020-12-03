8c7e36a72967bd316cce5985180fd40e97fae4e3
Mateusz Dąbrowski <mat.dabrowski@yahoo.com>
2020-12-03 20:19:52 +0100

    first commit

---
+++ src/test/java/com/kodilla/kodillagame/KodillaGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.kodillagame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodillaGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

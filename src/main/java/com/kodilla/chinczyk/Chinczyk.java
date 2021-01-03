0081d1aa81787a0e5338fb312577190f2f71bbf1
Mateusz Dąbrowski <mat.dabrowski@yahoo.com>
2021-01-03 09:48:13 +0100

    Without objects

--- src/main/java/com/kodilla/chinczyk/Chinczyk.java
+++ src/main/java/com/kodilla/chinczyk/Chinczyk.java
@@ -28,11 +28,15 @@ public class Chinczyk extends Application {
     private Pane throwButton = new Pane();
     private Pane labels = new Pane();
     Dice dice = new Dice();
-    public int diceNo;
-    int i = 786;
+    public int diceNo = 0;
     boolean isFirstMove = true;
-    int startPositionXForRed1 = 503;
-    int startPositionYForRed1 = 686;
+    boolean notHomeButtRed1 = false;
+    int homePositionXForRed1 = 503;
+    int homePositionYForRed1 = 686;
+    int startPositionXForRed1 = 604;
+    int startPositionYForRed1 = 786;
+    double actualPositionXForRed1;
+    double actualPositionYForRed1;
 
 
     public static void main(String[] args) {
@@ -42,8 +46,6 @@ public class Chinczyk extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-
-
 
         BackgroundSize backgroundSize = new BackgroundSize(100, 100, true, true, true, false);
         BackgroundImage backgroundImage = new BackgroundImage(imageback, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, backgroundSize);
@@ -59,18 +61,16 @@ public class Chinczyk extends Application {
         Button rzut = new Button("Throw a dice");
             rzut.setPrefSize(200, 50);
             rzut.setStyle("-fx-font-size:18");
-        Button pawnButton = new Button();
+        Button pawnButton = new Button("1");
             pawnButton.setStyle("-fx-background-radius: 55em; " + "-fx-min-width: 55px; " + "-fx-min-height: 55px; " + "-fx-max-width: 55px; " + "-fx-max-height: 55px;" + "-fx-background-color: #ff0000;");
-            pawnButton.setLayoutX(startPositionXForRed1);
-            pawnButton.setLayoutY(startPositionYForRed1);
-
+            pawnButton.setLayoutX(homePositionXForRed1);
+            pawnButton.setLayoutY(homePositionYForRed1);
         Label label = new Label();
             label.setStyle("-fx-font-size:18");
             label.setPrefSize(200,50);
             label.setMaxWidth(Double.MAX_VALUE);
             label.setAlignment(Pos.CENTER);
             label.autosize();
-
 
         ImageView img = new ImageView(imageDice);
             img.setFitHeight(250);
@@ -94,22 +94,56 @@ public class Chinczyk extends Application {
             } else {
                 img.setImage(new Image("file:src/main/resources/kosci3.png"));
             }
-
-
         };
 
         EventHandler<ActionEvent> makeMoveAction = new EventHandler<ActionEvent>() {
             public void handle(ActionEvent e)
             {
-                //i = i - 60;
-                // pierwszy ruch X = 604;
                 if (diceNo == 6 && isFirstMove)  {
                     pawnButton.setLayoutX(604);
                     pawnButton.setLayoutY(786);
+                    actualPositionXForRed1 = 604;
+                    actualPositionYForRed1 = 786;
+                    notHomeButtRed1 = true;
+
+
                 }
+                if (isFirstMove && notHomeButtRed1 && actualPositionXForRed1 != homePositionXForRed1 && actualPositionYForRed1 != homePositionYForRed1 ){
+                    isFirstMove = false;
+                } else {
+                    double tempNextPositionX = actualPositionXForRed1;
+                    double tempNextPositionY = actualPositionYForRed1;
+
+                    //if ((tempNextPositionX - diceNo * 67.5) < 604 && (tempNextPositionX - diceNo * 67.5) > 604)
+
+
+                    for (int n = 1; n <= diceNo; n++ ) {
+                        tempNextPositionX = tempNextPositionX;
+                        if (tempNextPositionY != 516){
+                            tempNextPositionY = tempNextPositionY - 67.5;
+                        }
+
+                        if (tempNextPositionX <= 604 && (tempNextPositionY == 516) ){
+                            tempNextPositionX = tempNextPositionX - 67.5;
+                            tempNextPositionY = 516;
+                        }
+//                        if (tempNextPositionX - (diceNo * 67.5) == 604 && tempNextPositionY == 516) {
+//                            tempNextPositionX = tempNextPositionX - (diceNo * 67.5);
+//                            tempNextPositionY = 516;
+//                        }
+
+                    }
+                    pawnButton.setLayoutX(tempNextPositionX);
+                    pawnButton.setLayoutY(tempNextPositionY);
+                    actualPositionXForRed1 = tempNextPositionX;
+                    actualPositionYForRed1 = tempNextPositionY;
+                    rzut.fire();
+
+                }
+
+
             }
         };
-
 
         rzut.setOnAction(event);
         pawnButton.setOnAction(makeMoveAction);
@@ -127,13 +161,10 @@ public class Chinczyk extends Application {
         grid.getChildren().add(pawnButton);
         Scene scene = new Scene(grid, 1400, 950, Color.WHITESMOKE);
 
-
-
         primaryStage.setTitle("Chinczyk");
         primaryStage.setScene(scene);
         primaryStage.show();
-
-
+        primaryStage.setResizable(false);
 
     }
 }
---
+++ src/main/java/com/kodilla/kolkokrzyzyk/KolkoKrzyzykGame.java
@@ -0,0 +1,422 @@
+package com.kodilla.kolkokrzyzyk;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+import javax.swing.*;
+import javax.swing.Timer;
+import java.util.*;
+import java.util.concurrent.Flow;
+
+public class KolkoKrzyzykGame extends Application {
+    private Image imageback = new Image("file:src/main/resources/ttt.png");
+    private Pane dices = new Pane();
+    private Pane throwButton = new Pane();
+    private Pane labels = new Pane();
+    String whosTurn = "X";
+    private void checkWhosTurn(){
+        if (whosTurn == "X") {
+            whosTurn = "O";
+        } else {
+            whosTurn = "X";
+        }
+    }
+    Random randomInt = new Random();
+    int randInt = 0;
+    private String xOrObutt1;
+    private String xOrObutt2;
+    private String xOrObutt3;
+    private String xOrObutt4;
+    private String xOrObutt5;
+    private String xOrObutt6;
+    private String xOrObutt7;
+    private String xOrObutt8;
+    private String xOrObutt9;
+    private HashSet<Button> disableButtons = new HashSet<>();
+    private Alert alertBox = new Alert(Alert.AlertType.INFORMATION);
+
+    private void checkWhosWin() {
+        if(disableButtons.size() == 9) {
+            alertBox.setContentText("Nobody win :(");
+            alertBox.show();
+        }
+
+        if (xOrObutt1 == "X" && xOrObutt2 == "X" && xOrObutt3 == "X") {
+            alertBox.setContentText("The winner is: X");
+            alertBox.show();
+        }
+        if (xOrObutt1 == "O" && xOrObutt2 == "O" && xOrObutt3 == "O") {
+            alertBox.setContentText("The winner is: O");
+            alertBox.show();
+        }
+        if (xOrObutt4 == "X" && xOrObutt5 == "X" && xOrObutt6 == "X") {
+            alertBox.setContentText("The winner is: X");
+            alertBox.show();
+        }
+        if (xOrObutt4 == "O" && xOrObutt5 == "O" && xOrObutt6 == "O") {
+            alertBox.setContentText("The winner is: O");
+            alertBox.show();
+        }
+        if (xOrObutt7 == "X" && xOrObutt8 == "X" && xOrObutt9 == "X") {
+            alertBox.setContentText("The winner is: X");
+            alertBox.show();
+        }
+        if (xOrObutt7 == "O" && xOrObutt8 == "O" && xOrObutt9 == "O") {
+            alertBox.setContentText("The winner is: O");
+            alertBox.show();
+        }
+        if (xOrObutt7 == "X" && xOrObutt8 == "X" && xOrObutt9 == "X") {
+            alertBox.setContentText("The winner is: X");
+            alertBox.show();
+        }
+        if (xOrObutt1 == "X" && xOrObutt4 == "X" && xOrObutt7 == "X") {
+            alertBox.setContentText("The winner is: X");
+            alertBox.show();
+        }
+        if (xOrObutt1 == "O" && xOrObutt4 == "O" && xOrObutt7 == "O") {
+            alertBox.setContentText("The winner is: O");
+            alertBox.show();
+        }
+        if (xOrObutt2 == "X" && xOrObutt5 == "X" && xOrObutt8 == "X") {
+            alertBox.setContentText("The winner is: X");
+            alertBox.show();
+        }
+        if (xOrObutt2 == "O" && xOrObutt5 == "O" && xOrObutt8 == "O") {
+            alertBox.setContentText("The winner is: O");
+            alertBox.show();
+        }
+        if (xOrObutt3 == "X" && xOrObutt6 == "X" && xOrObutt9 == "X") {
+            alertBox.setContentText("The winner is: X");
+            alertBox.show();
+        }
+        if (xOrObutt3 == "O" && xOrObutt6 == "O" && xOrObutt9 == "O") {
+            alertBox.setContentText("The winner is: O");
+            alertBox.show();
+        }
+        if (xOrObutt1 == "X" && xOrObutt5 == "X" && xOrObutt9 == "X") {
+            alertBox.setContentText("The winner is: X");
+            alertBox.show();
+        }
+        if (xOrObutt1 == "O" && xOrObutt5 == "O" && xOrObutt9 == "O") {
+            alertBox.setContentText("The winner is: O");
+            alertBox.show();
+        }
+        if (xOrObutt3 == "X" && xOrObutt5 == "X" && xOrObutt7 == "X") {
+            alertBox.setContentText("The winner is: X");
+            alertBox.show();
+        }
+        if (xOrObutt3 == "O" && xOrObutt5 == "O" && xOrObutt7 == "O") {
+            alertBox.setContentText("The winner is: O");
+            alertBox.show();
+        }
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        BackgroundSize backgroundSize = new BackgroundSize(100, 100, true, true, true, false);
+        BackgroundImage backgroundImage = new BackgroundImage(imageback, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, backgroundSize);
+        Background background = new Background(backgroundImage);
+
+        Pane grid = new Pane();
+        grid.setPadding(new Insets(11.5, 12.5, 13.5, 14.5));
+        grid.setBackground(background);
+
+
+        Button[] button = new Button[10];
+        button[1] = new Button();
+        button[1].setPrefSize(150, 150);
+        button[1].setStyle("-fx-font-size:30");
+        button[1].setLayoutX(100);
+        button[1].setLayoutY(100);
+
+        button[2] = new Button();
+        button[2].setPrefSize(150, 150);
+        button[2].setStyle("-fx-font-size:30");
+        button[2].setLayoutX(325);
+        button[2].setLayoutY(100);
+
+        button[3] = new Button();
+        button[3].setPrefSize(150, 150);
+        button[3].setStyle("-fx-font-size:30");
+        button[3].setLayoutX(550);
+        button[3].setLayoutY(100);
+
+        button[4] = new Button();
+        button[4].setPrefSize(150, 150);
+        button[4].setStyle("-fx-font-size:30");
+        button[4].setLayoutX(100);
+        button[4].setLayoutY(325);
+
+        button[5] = new Button();
+        button[5].setPrefSize(150, 150);
+        button[5].setStyle("-fx-font-size:30");
+        button[5].setLayoutX(325);
+        button[5].setLayoutY(325);
+
+        button[6] = new Button();
+        button[6].setPrefSize(150, 150);
+        button[6].setStyle("-fx-font-size:30");
+        button[6].setLayoutX(550);
+        button[6].setLayoutY(325);
+
+        button[7] = new Button();
+        button[7].setPrefSize(150, 150);
+        button[7].setStyle("-fx-font-size:30");
+        button[7].setLayoutX(100);
+        button[7].setLayoutY(550);
+
+        button[8] = new Button();
+        button[8].setPrefSize(150, 150);
+        button[8].setStyle("-fx-font-size:30");
+        button[8].setLayoutX(325);
+        button[8].setLayoutY(550);
+
+        button[9] = new Button();
+        button[9].setPrefSize(150, 150);
+        button[9].setStyle("-fx-font-size:30");
+        button[9].setLayoutX(550);
+        button[9].setLayoutY(550);
+
+        Label label = new Label();
+        label.setStyle("-fx-font-size:30");
+        label.setPrefSize(200,50);
+        label.setMaxWidth(Double.MAX_VALUE);
+        label.setAlignment(Pos.CENTER);
+        label.autosize();
+        label.setLayoutX(310);
+        label.setLayoutY(20);
+        label.setText("It is " + whosTurn + " turn.");
+
+        Button compMovebutton = new Button();
+
+
+        EventHandler<ActionEvent> compMove = new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent e)
+            {
+                int randInt = randomInt.nextInt(10);
+                if (disableButtons.contains(button[randInt]) || randInt == 0) {
+                    compMovebutton.fire();
+                } else {
+                    button[randInt].fire();
+                }
+            }
+        };
+
+        Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(1), new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                int randInt = randomInt.nextInt(10);
+                if (disableButtons.contains(button[randInt]) || randInt == 0) {
+                    compMovebutton.fire();
+                } else {
+                    button[randInt].fire();
+                }
+            }
+        }));
+
+
+        EventHandler<ActionEvent> button1Event = new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent e)
+            {
+                button[1].setText(whosTurn);
+                button[1].setDisable(true);
+                xOrObutt1 = whosTurn;
+                disableButtons.add(button[1]);
+                checkWhosTurn();
+                label.setText("It is " + whosTurn + " turn.");
+                checkWhosWin();
+                if(whosTurn == "O") {
+                    timeline.play();
+                    checkWhosWin();
+                }
+
+            }
+        };
+
+        EventHandler<ActionEvent> button2Event = new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent e)
+            {
+                button[2].setText(whosTurn);
+                button[2].setDisable(true);
+                xOrObutt2 = whosTurn;
+                disableButtons.add(button[2]);
+                checkWhosTurn();
+                label.setText("It is " + whosTurn + " turn.");
+                checkWhosWin();
+                if(whosTurn == "O") {
+                    timeline.play();
+                    checkWhosWin();
+                }
+            }
+        };
+
+        EventHandler<ActionEvent> button3Event = new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent e)
+            {
+                button[3].setText(whosTurn);
+                button[3].setDisable(true);
+                xOrObutt3 = whosTurn;
+                disableButtons.add(button[3]);
+                checkWhosTurn();
+                label.setText("It is " + whosTurn + " turn.");
+                checkWhosWin();
+                if(whosTurn == "O") {
+                    timeline.play();
+                    checkWhosWin();
+                }
+            }
+        };
+
+        EventHandler<ActionEvent> button4Event = new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent e)
+            {
+                button[4].setText(whosTurn);
+                button[4].setDisable(true);
+                xOrObutt4 = whosTurn;
+                disableButtons.add(button[4]);
+                checkWhosTurn();
+                label.setText("It is " + whosTurn + " turn.");
+                checkWhosWin();
+                if(whosTurn == "O") {
+                    timeline.play();
+                    checkWhosWin();
+                }
+            }
+        };
+
+        EventHandler<ActionEvent> button5Event = new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent e)
+            {
+                button[5].setText(whosTurn);
+                button[5].setDisable(true);
+                xOrObutt5 = whosTurn;
+                disableButtons.add(button[5]);
+                checkWhosTurn();
+                label.setText("It is " + whosTurn + " turn.");
+                checkWhosWin();
+                if(whosTurn == "O") {
+                    timeline.play();
+                    checkWhosWin();
+                }
+            }
+        };
+
+        EventHandler<ActionEvent> button6Event = new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent e)
+            {
+                button[6].setText(whosTurn);
+                button[6].setDisable(true);
+                xOrObutt6 = whosTurn;
+                disableButtons.add(button[6]);
+                checkWhosTurn();
+                label.setText("It is " + whosTurn + " turn.");
+                checkWhosWin();
+                if(whosTurn == "O") {
+                    timeline.play();
+                    checkWhosWin();
+                }
+            }
+        };
+
+        EventHandler<ActionEvent> button7Event = new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent e)
+            {
+                button[7].setText(whosTurn);
+                button[7].setDisable(true);
+                xOrObutt7 = whosTurn;
+                disableButtons.add(button[7]);
+                checkWhosTurn();
+                label.setText("It is " + whosTurn + " turn.");
+                checkWhosWin();
+                if(whosTurn == "O") {
+                    timeline.play();
+                    checkWhosWin();
+                }
+            }
+        };
+
+        EventHandler<ActionEvent> button8Event = new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent e)
+            {
+                button[8].setText(whosTurn);
+                button[8].setDisable(true);
+                xOrObutt8 = whosTurn;
+                disableButtons.add(button[8]);
+                checkWhosTurn();
+                label.setText("It is " + whosTurn + " turn.");
+                checkWhosWin();
+                if(whosTurn == "O") {
+                    timeline.play();
+                    checkWhosWin();
+                }
+            }
+        };
+
+        EventHandler<ActionEvent> button9Event = new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent e)
+            {
+                button[9].setText(whosTurn);
+                button[9].setDisable(true);
+                xOrObutt9 = whosTurn;
+                disableButtons.add(button[9]);
+                checkWhosTurn();
+                label.setText("It is " + whosTurn + " turn.");
+                checkWhosWin();
+                if(whosTurn == "O") {
+                    timeline.play();
+                    checkWhosWin();
+                }
+            }
+        };
+
+        button[1].setOnAction(button1Event);
+        button[2].setOnAction(button2Event);
+        button[3].setOnAction(button3Event);
+        button[4].setOnAction(button4Event);
+        button[5].setOnAction(button5Event);
+        button[6].setOnAction(button6Event);
+        button[7].setOnAction(button7Event);
+        button[8].setOnAction(button8Event);
+        button[9].setOnAction(button9Event);
+        compMovebutton.setOnAction(compMove);
+
+
+        grid.getChildren().add(button[1]);
+        grid.getChildren().add(button[2]);
+        grid.getChildren().add(button[3]);
+        grid.getChildren().add(button[4]);
+        grid.getChildren().add(button[5]);
+        grid.getChildren().add(button[6]);
+        grid.getChildren().add(button[7]);
+        grid.getChildren().add(button[8]);
+        grid.getChildren().add(button[9]);
+        grid.getChildren().add(label);
+        Scene scene = new Scene(grid, 800, 800, Color.WHITESMOKE);
+
+        primaryStage.setTitle("Kółko i krzyżyk");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        primaryStage.setResizable(false);
+
+    }
+}
+
